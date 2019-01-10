73d824a9a1524e2053315e4363c8a902ae0a7ec7
Simon Bates <simon@bitstructures.com>
2019-01-10 11:20:30 -0500

    CITY-42: Copy only the parts of Infusion needed by UIO

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,31 @@ module.exports = function (grunt) {
         copy: {
             uio: {
                 files: [
-                    // Copy Infusion
+                    // Copy Infusion source files needed by UIO
                     {
                         expand: true,
                         cwd: "node_modules/infusion/",
-                        src: "**",
+                        src: [
+                            "src/components/tableOfContents/**",
+                            "src/framework/core/**",
+                            "src/framework/preferences/**",
+                            "src/lib/normalize/**",
+                            "src/lib/opensans/**"
+                        ],
+                        dest: "static/js/lib/infusion/"
+                    },
+                    // Copy the UIO Infusion build
+                    {
+                        expand: true,
+                        cwd: "node_modules/infusion/dist/",
+                        src: "infusion-uio.min.js",
+                        dest: "static/js/lib/infusion/"
+                    },
+                    // Overlay the generated assets
+                    {
+                        expand: true,
+                        cwd: "node_modules/infusion/dist/assets/",
+                        src: "src/framework/preferences/**",
                         dest: "static/js/lib/infusion/"
                     }
                 ]
